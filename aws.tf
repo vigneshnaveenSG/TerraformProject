de08da5b0e4b4bfdfcb5f3bcd52f1ca93ea7d3f5
vigneshnaveenSG <108710692+vigneshnaveenSG@users.noreply.github.com>
2022-08-04 10:15:33 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "vignesh-web" {
+   ami           =  "ami-08df646e18b182346"
+  instance_type = "t2.micro"
+  count = 1
+  subnet_id = "subnet-0d47c7cc17ec8f644"
+
+  tags = {
+    Name = "Vignesh-web"
+  }
+}
